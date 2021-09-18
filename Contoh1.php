40d085e9aa362c9d0e8eacf7d35fbf857ac05810
farhanauan <farhanauan@gmail.com>
2021-09-18 11:36:01 +0700

    penambahan Contoh1.php untuk tugas1 WP2

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Hai Nama Saya Farhan Gazalba
+            Saya hobi Bermain Game dan bersepdah
+            dan Saya Berkuliah Di UBSI";
+    }
+}
